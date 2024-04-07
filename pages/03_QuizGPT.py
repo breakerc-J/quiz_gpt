4cd459028062ab43c0315da1a596e3cba546a98b
breakerc-J <60805250+breakerc-J@users.noreply.github.com>
2024-04-07 23:19:11 +0900

    'quiz_gpt_bot'

--- pages/03_QuizGPT.py
+++ pages/03_QuizGPT.py
@@ -57,7 +57,7 @@ memory_llm = None
 llm = ChatOpenAI(
     temperature=0.1,
     model="gpt-3.5-turbo-0125",
-    aopenai_api_key=api_key,
+    openai_api_key=api_key,
     streaming=True,
     callbacks=[StreamingStdOutCallbackHandler()],
    )
